7274e2a8019ed1c9d02e00efa32075bed5d01757
MidHost1 <maksim.frizorher123@gmail.com>
2025-02-09 14:05:21 +0100

    Виправив завдання №2 і №3

--- module_3_homework_2.py
+++ module_3_homework_2.py
@@ -11,6 +11,13 @@
 '''
 from random import randint
 def get_numbers_ticket(min, max, quantity):
+    if min > max or quantity > (max - min + 1) or quantity <= 0: # Додаємо перевірку вхідних значень
+        return []
+    elif min < 0:
+        return []
+    elif max > 1000:
+        return []
+
     num_list = [] # Створюємо список, де будемо зберігати наші значення
     while len(num_list) < quantity:
         num = randint(min, max)
@@ -18,11 +25,15 @@ def get_numbers_ticket(min, max, quantity):
         if num not in num_list: # Перевірка на дублікат чисел у списку, щоб він приймав тільки унікальні значення
             num_list.append(num)
 
-    return num_list
+    return sorted(num_list)
 
 # Перевіряємо функцію
-print(get_numbers_ticket(1, 159, 4))
-
+print(get_numbers_ticket(1, 49, 6))
+# Перевіряємо справність нашої перевірки вхідних значень
+print(get_numbers_ticket(-10, 10, 5))
+print(get_numbers_ticket(1000, 1200, 10))
+print(get_numbers_ticket(10, 4, 5))
+print(get_numbers_ticket(10, 14, 6))
 '''
 Потім прочитав і згадав, що також можна це було зробити за допомогою random.sample(range(min, max + 1), quantity). Це набагато практичніше
 '''
--- module_3_homework_3.py
+++ module_3_homework_3.py
@@ -5,36 +5,35 @@
 Розробіть функцію normalize_phone(phone_number), що нормалізує телефонні номери до стандартного формату, залишаючи тільки цифри та символ '+' на початку.
 '''
 import re
-raw_numbers = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
 
 def normalize_phone(phone_number):
-    cleared_numbers = []
-    for phone in phone_number:
-        updated_number = re.sub(r"[^0-9]", "", phone)# Видаляємо все, крім цифр
-        
-        #Тут ми додаємо код країни, якщо його немає, за допомогою умовного оператора "if"
-        if updated_number.startswith('380'):
-            updated_number = '+' + updated_number # Тут - додали "+"
-            
-        else:
-            updated_number = '+38' + updated_number # Тут перевіряється випадок, коли номер не починається з "380", а з "0", як деякі номери зі списку
-        
-        cleared_numbers.append(updated_number)
-
-
-
-    return cleared_numbers
     
-sanitized_numbers = normalize_phone(raw_numbers)
-print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+    
+    updated_number = re.sub(r"[^0-9]", "", phone_number)# Видаляємо все, крім цифр
+        
+    #Тут ми додаємо код країни, якщо його немає, за допомогою умовного оператора "if"
+    if updated_number.startswith("380"):
+        updated_number = '+' + updated_number # Тут - додали "+"
+            
+    elif updated_number.startswith("0"):
+        updated_number = '+38' + updated_number # Тут перевіряється випадок, коли номер не починається з "380", а з "0", як деякі номери зі списку
 
+    else:
+        return "Номер не відповідає укр. формату" # Якщо номер не відповідає українському формату, виводимо ось таке повідомлення
+    
+
+
+
+    return updated_number
+# Перевіряємо перевірку, чи вказаний номер відповідає українському формату:
+print(normalize_phone("+49 161 71777965")) # Виводить - "Номер не відповідає укр. формату."
+# Перевіряємо функцію
+print(normalize_phone("067\\t123 4567"))
+print(normalize_phone("(095) 234-5678\\n"))
+print(normalize_phone("+380 44 123 4567"))
+print(normalize_phone("380501234567"))
+print(normalize_phone("    +38(050)123-32-34"))
+print(normalize_phone("     0503451234"))
+print(normalize_phone("(050)8889900"))
+print(normalize_phone("38050-111-22-22"))
+print(normalize_phone("38050 111 22 11   "))
